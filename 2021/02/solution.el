5d5ba7f3972b5234f352722ee6a8ac91589db786
Stefan Lendl <ste.lendl@gmail.com>
2021-12-02 21:49:21 +0100

    day 2 two versions

--- 2021/02/solution.el
+++ 2021/02/solution.el
@@ -6,6 +6,67 @@
 
 (require 'dash)
 
+;; (setq-local input (-map #'split-string (read-lines "example.txt")))
 (setq-local input (-map #'split-string (read-lines "input.txt")))
 
-(-group-by #'car input)
+;; Part 1
+(defun movements (list)
+  (-map (-compose 'string-to-number 'car 'cdr) list))
+
+(defun sum-movements (list)
+  (-sum (movements list)))
+
+(setq-local grouped (-group-by #'car input))
+
+(setq-local depth (- (sum-movements (cdr (assoc "down" grouped)))
+                     (sum-movements (cdr (assoc "up" grouped)))))
+
+(setq-local forward (sum-movements (cdr (assoc "forward" grouped))))
+
+(setq-local sum-forward-depth (* depth forward))
+
+(message "Part 1: %d" sum-forward-depth)
+
+;; Part 2
+
+(defun gen-dir-list (el)
+  (let ((direction (car el))
+        (num (string-to-number (car (cdr el)))))
+    ; (forward direction)
+    (cond ((string= direction "down") `(nil ,num))
+          ((string= direction "up") `(nil ,(- num)))
+          ((string= direction "forward") `(,num nil)))))
+
+(setq zipped (-unzip (-map 'gen-dir-list input))
+      forward-list (car zipped)
+      depth-list (car (cdr zipped)))
+
+(setq aim-acc (-running-sum (-replace nil 0 depth-list)))
+
+(setq depth-aimed
+      (-sum (-map (lambda (el)
+                    (if (-cons-pair? el) (* (car el) (cdr el)) 0))
+                  (-zip aim-acc forward-list))))
+
+
+(setq prod-aimed (* depth-aimed (-sum (-replace nil 0 forward-list))))
+
+(message "Part 2: %d" prod-aimed)
+
+;; using -reduce-from
+
+(-product
+ (cdr
+  (-reduce-from
+   (lambda (acc el)
+     (let ((direction (car el))
+           (num (string-to-number (elt el 1))))
+       (cond ((string= direction "down") (cl-incf (elt acc 0) num))
+             ((string= direction "up") (cl-decf (elt acc 0) num))
+             ((string= direction "forward")
+              (cl-incf (elt acc 1) (* num (elt acc 0)))
+              (cl-incf (elt acc 2) num))
+             )
+       acc))
+   '(0 0 0) ;; (aim depth forward)
+   input)))
