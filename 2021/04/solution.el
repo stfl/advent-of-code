18d249df3eb8ca4973d0be01109558c989ee4ad7
Stefan Lendl <ste.lendl@gmail.com>
2021-12-07 23:23:15 +0100

    day 4 cleaning up

--- 2021/04/solution.el
+++ 2021/04/solution.el
@@ -12,38 +12,11 @@
 (setq-local input (read-lines "input.txt"))
 
 (setq-local in-list (split-string (car input) ","))
-
-(setq-local boards
-            (-map 'split-string
-                  (-filter (-not 'string-empty-p)
-                           (cdr input))))
+(setq-local boards (-partition 5 (-map 'split-string (cdr input))))
 
 (defun board-complete? (board)
   (-first (-partial #'-all? 'null)
           (append board (-unzip board))))
-
-(defun mark-until-found (in-list boards)
-  (let* ((marked boards)
-         (f (catch 'found
-              (dolist (in in-list)
-                (setq marked (-map (-partial '-replace in 'nil) marked))
-                (let ((completed-board (-first 'board-complete? (-partition 5 marked))))
-                  (when completed-board
-                    (throw 'found `(,in . ,completed-board))))))))
-    f))
-
-(defun score (in-list boards)
-  (-let (((last-in . board) (mark-until-found in-list boards)))
-    (* (string-to-number last-in)
-       (-sum (-map 'string-to-number (-flatten board))))))
-
-(message "Part1: %s" (score in-list boards))
-
-;; Part 2
-
-;; without catch throw
-
-(setq boards (-partition 5 boards))
 
 (defun mark-boards (boards in)
   (-tree-map (lambda (el) (unless (string= el in) el)) boards))
@@ -55,15 +28,15 @@
       (cons completed-board
             (play-bingo remaining (cdr in-list))))))
 
-(defun bingo-score (in board)
+(defun bingo-score (board in)
   (when board
     (* (string-to-number in)
        (-sum (-map 'string-to-number (-flatten board))))))
 
 (defun bingo-scores (boards in-list)
   (-map (-applify #'bingo-score)
-        (-zip-lists in-list
-                    (play-bingo boards in-list))))
+        (-zip-lists (play-bingo boards in-list)
+                    in-list)))
 
-;; (message "Part1: %d" (-first (-not 'null) (bingo-scores boards in-list)))
+(message "Part1: %d" (-first (-not 'null) (bingo-scores boards in-list)))
 (message "Part2: %d" (-last (-not 'null) (bingo-scores boards in-list)))
